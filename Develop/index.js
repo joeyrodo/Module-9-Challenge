771b5a4ba2c92ce9abd04996406d40d81ea3d5ea
joeyrodo <joeyrodo@gmail.com>
2023-04-27 20:43:01 -0400

    Added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,63 @@
 // TODO: Include packages needed for this application\
-const inquirer = import('inquirer');
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
+let questions = [
+    {
+        // TEST QUESTION
+        type: 'input',
+        name: 'givenName',
+        message: 'Enter your given name: '
+    },
+    {
+        // TITLE QUESTION
+        type: 'input',
+        name: 'title',
+        message: 'Enter the title of the project: '
+    },
+    {
+        // DESCRIPTION QUESTION
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description for the project: '
+    },
+    {
+        // USAGE QUESTION
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information: '
+    },
+    {
+        // CONTRIBUTION QUESTION
+        type: 'input',
+        name: 'contribution',
+        message: 'Enter contribution guidelines: '
+    },
+    {
+        // TESTS QUESTION
+        type: 'input',
+        name: 'test',
+        message: 'Enter the way(s) a user can test the program: '
+    },
+    {
+        // GITHUB USERNAME QUESTION
+        type: 'input',
+        name: 'username',
+        message: 'Enter your GitHub username (for "questions" section): '
+    },
+    {
+        // EMAIL ADDRESS QUESTION
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address (for "questions" section): '
+    },
+    {
+        // LICENSE QUESTION
+        type: 'list',
+        name: 'license',
+        choices: [ 'Apache License v2.0', 'GNU General Public License v3.0', 'MIT License', 'no license']
+    }
 
+]
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
@@ -10,5 +66,11 @@ function init() {}
 
 // Function call to initialize app
 init();
-
+// tests
 console.log("hello");
+
+inquirer.prompt(questions)
+    .then(answers => {
+      console.log(answers.givenName);
+    })
+
